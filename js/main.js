430493e7bad75b0c5528384c649f8daa3ed2e67b
Marcin Kwiatkowski <marian.kwietniewicz@gmail.com>
2018-05-24 15:03:29 +0200

    poprawione-ale-nie-do-konca

--- js/main.js
+++ js/main.js
@@ -24,7 +24,13 @@ $(function() {
        }
     });
     
-    
+    $(window).scroll(function(){
+       if ($(window).scrollTop() > 50){
+           $('#main-nav').addClass('scroll');
+       } else {
+           $('#main-nav').removeClass('scroll');
+       }
+    });
     
     
     
